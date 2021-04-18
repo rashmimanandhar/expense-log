d5ab88e03edb5737e7b3ea59e638ac3cd9955f8d
Rashmi <rashmi.manandhar7@gmail.com>
2021-04-18 15:12:56 -0400

    feat: create a form to enter expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (year) => {
+    setFilteredYear(year);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onYearChanged={filterChangeHandler}
+      />
+
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
